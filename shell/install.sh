4441e86b7cf17a70200c2735c8657cae2cf61a4b
Kirill akinevz Nevzorov <akinevz@outlook.com>
2022-03-27 23:55:12 +0100

    install plugins for zsh script

---
+++ shell/install.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+

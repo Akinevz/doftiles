ee89335dd6b70804914bfec7679b250013991d6b
Kirill akinevz Nevzorov <akinevz@outlook.com>
2022-03-27 23:56:26 +0100

    update install script to install zsh systems

--- shell/install.sh
+++ shell/install.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#
+#
+# install on arch - like systems
+
+yay -S oh-my-zsh-git zsh-theme-powerlevel10k-git
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
